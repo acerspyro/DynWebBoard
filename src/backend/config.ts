b896ffed13ef9f276bb8e13ad29152f32cb8a685
VeryLoudMonstercat <maxim@acerspyro.ca>
2019-09-14 23:32:48 -0400

    This is peak callback hell

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -8,32 +8,33 @@ class Config {
         this.path = `${__root}/${path}`;
         this.configObject = Object();
 
-        this.configObject = this.loadConfig();
+        this.configObject = this.loadConfig(this.path);
     }
 
-    initConfig(path: string = this.path) {
+    async initConfig(path: string = this.path) {
         let defaultConfig = {
             databasePath: 'DynWebBoard.sqlite',
             uploadPath: 'uploads'
         };
 
-        return fs.writeFile(path, JSON.stringify(defaultConfig), (e: any) => {
+        return fs.writeFile(path, 'utf8', JSON.stringify(defaultConfig), (e: any) => {
             if (e)  console.error(`[E] Unable to create config file => ${e}`);
             else    console.info(`[I] Created default config file @ '${path}'`);
         });
     }
 
-    async loadConfig(path: string = this.path) {
-        fs.readFile(path, (e: any) => {
+    async loadConfig(path: string = this.path, callback: Function) { // Callback: (data)
+        return fs.readFile(path, 'utf8', async (e: any, data: any) => {
             if (e)  {
                 try {
                     fs.accessSync(path, fs.constants.F_OK);
                     console.error(`[E] Unable to read config file => ${e}`);
                 } catch {
-                    return this.initConfig();
+                    return callback(await this.initConfig());
                 }
             } else {
                 console.info(`[I] Read config file @ '${path}'`);
+                return data;
             }
         });
     }
